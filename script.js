020adc6bcd9defcd885102090f4c3e0c74eb7c54
Aaron Kagan <aaronkagan@yahoo.com>
2023-11-03 20:28:15 -0400

    fixing price bug

--- script.js
+++ script.js
@@ -6,30 +6,36 @@ const toggleButtonInner = document.querySelector('.toggle-inner');
 let isMonthly = true;
 let range = '3';
 
-const applyDiscount = (dynamicPrice) => {
-  dynamicPrice.innerText = parseInt(dynamicPrice.innerText) * 0.75;
-};
-
 const setPrice = (range) => {
   switch (range) {
     case '1':
-      isMonthly ? (dynamicPrice.innerText = '8') : applyDiscount(dynamicPrice);
+      isMonthly
+        ? (dynamicPrice.innerText = '8')
+        : (dynamicPrice.innerText = '6');
       numPageviews.innerText = '10k';
       break;
     case '2':
-      isMonthly ? (dynamicPrice.innerText = '12') : applyDiscount(dynamicPrice);
+      isMonthly
+        ? (dynamicPrice.innerText = '12')
+        : (dynamicPrice.innerText = '8');
       numPageviews.innerText = '50k';
       break;
     case '3':
-      isMonthly ? (dynamicPrice.innerText = '16') : applyDiscount(dynamicPrice);
+      isMonthly
+        ? (dynamicPrice.innerText = '16')
+        : (dynamicPrice.innerText = '12');
       numPageviews.innerText = '100k';
       break;
     case '4':
-      isMonthly ? (dynamicPrice.innerText = '24') : applyDiscount(dynamicPrice);
+      isMonthly
+        ? (dynamicPrice.innerText = '24')
+        : (dynamicPrice.innerText = '18');
       numPageviews.innerText = '500k';
       break;
     case '5':
-      isMonthly ? (dynamicPrice.innerText = '36') : applyDiscount(dynamicPrice);
+      isMonthly
+        ? (dynamicPrice.innerText = '36')
+        : (dynamicPrice.innerText = '27');
       numPageviews.innerText = '1m';
       break;
     default:
